b749c452f9e87d18d491164b844702b0963f4df1
OlgaP-1 <72099983+OlgaP-1@users.noreply.github.com>
2021-01-20 17:36:23 +0300

    Add files via upload

---
+++ 7-1. shop_cosmetic_представления.sql
@@ -0,0 +1,17 @@
+/*представление показывает ФИО и возраст покупателей, заказавших 30 самых популярных товаров, по лайкам посетителей. */
+
+USE `shop_cosmetics`;
+CREATE  OR REPLACE VIEW `produc_tb1` AS
+SELECT 
+    product.id, product.decode, product.price,
+    (SELECT COUNT(likes.product_id) as c_pro FROM shop_cosmetics.likes WHERE likes.product_id = product.id GROUP BY likes.product_id) AS c_likes, 
+    orders.product_id,
+    orders.users_id, 
+    CONCAT(users.firstname, ' ', users.lastname) AS name_us,
+    timestampdiff(year, profiles.birthday, NOW()) AS age
+FROM
+    shop_cosmetics.product 
+    JOIN shop_cosmetics.orders ON orders.product_id = product.id 
+    JOIN shop_cosmetics.users ON orders.users_id = users.id
+    JOIN shop_cosmetics.profiles ON orders.users_id = profiles.users_id
+    order by c_likes desc limit 30;
